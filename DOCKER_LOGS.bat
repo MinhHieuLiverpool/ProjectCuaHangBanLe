9c6d43273e19a1529406515e6f5e05880e7626f4
haole2k4 <ahihihaodepzai@gmail.com>
2025-10-21 14:24:30 +0700

    del: logs, scripts don't use and some markdown files
    làm sạch dự án

--- DOCKER_LOGS.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ====================================
-echo Viewing Docker Container Logs...
-echo ====================================
-echo Press Ctrl+C to exit
-echo ====================================
-echo.
-
-docker-compose logs -f
